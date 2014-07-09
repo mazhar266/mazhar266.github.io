c1d23cd994c3b0547bc4968a762c366f499daa7c
Mazhar Ahmed <mazhar266@gmail.com>
2014-07-09 11:04:08 +0600

    removed new themes

--- rejected/coffee/main.js
+++
@@ -1,1 +0,0 @@
-(function(){var i,e;i=jQuery,e=i(".hero"),i(function(){return e.backstretch(["img/slides/1.jpg","img/slides/3.jpg","img/slides/4.jpg","img/slides/6.jpg"],{duration:3e3,fade:750})})}).call(this);
