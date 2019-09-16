e8c08390aeaa8c4dfcf1ceb0b17dd7dc66b28707
Mazhar Ahmed <mazhar@seazoobd.com>
2019-09-16 14:38:06 +0600

    updated the eloan page

--- shopup-eloan/js/app.js
+++ shopup-eloan/js/app.js
@@ -1,9 +1,37 @@
-var myApp = angular.module('eloan', ['rzSlider']);
+var myApp = angular.module('eloan', ['ngSanitize', 'rzSlider']);
 
-myApp.controller('AppController', function () {
-    var vm = this;
-    vm.loanAmount = 27000
-    vm.loanDuration = 5
+myApp.controller('AppController', function ($scope) {
+    $scope.loanAmount = 27000;
+    $scope.loanAmountOption = {
+        floor: 30000,
+        ceil: 300000,
+        step: 1000,
+        showSelectionBar: true,
+        hidePointerLabels: true,
+        hideLimitLabels: true,
+        ticksArray: [
+            30000,
+            60000,
+            90000,
+            120000,
+            150000,
+            180000,
+            210000,
+            240000,
+            270000,
+            300000,
+        ]
+    };
+
+    $scope.loanDuration = 5;
+    $scope.loanDurationOption = {
+        floor: 1,
+        ceil: 36,
+        showSelectionBar: true,
+        hidePointerLabels: true,
+        hideLimitLabels: true,
+        ticksArray: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36]
+    };
 });
 
 myApp.config(function($rootScopeProvider){
